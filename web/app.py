43852414f640da3c63dd061adfda1ba6820509db
bamboo51 <pongsatorn2548self@gmail.com>
2024-11-04 09:29:44 +0200

    renew

---
+++ web/app.py
@@ -0,0 +1,42 @@
+from flask import Flask, Response, render_template, send_file
+from flask_bootstrap import Bootstrap5
+import sys
+sys.path.append("../")
+from sensors.camera_stream import generate_frames, streaming_event
+import os
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+NO_STREAM_IMAGE_PATH = "./static/image.png"
+'''
+def start_camera_streaming():
+    if not streaming_event.is_set():
+        streaming_event.set()
+'''
+        
+# Serve the HTML page with video stream
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/streaming')
+def streaming():
+    return render_template('streaming.html')
+
+@app.route('/video_feed')
+def video_feed():
+    """Video streaming route. Put this in the src attribute of an img tag"""
+    if streaming_event.is_set():
+        return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+    '''
+    else:
+        if os.path.exists(NO_STREAM_IMAGE_PATH):
+            return send_file(NO_STREAM_IMAGE_PATH, mimetype='image/png')
+        else:
+            return "No stream available"
+    '''
+
+def run_flask():
+    app.run(host='0.0.0.0', port=5000, debug=False)
+
