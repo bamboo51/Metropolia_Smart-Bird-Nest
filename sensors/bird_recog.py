97abf0c5cd6217f825dfd6951772319b9cf0169e
bamboo51 <pongsatorn2548self@gmail.com>
2024-11-21 16:10:37 +0200

    update audio record

--- sensors/bird_recog.py
+++ sensors/bird_recog.py
@@ -19,10 +19,29 @@ def save_to_database(timestamp, bird_species, audio_path, image_path=None):
 def analyze_bird_sound(audio_path, timestamp):
     recording = Recording(analyzer, audio_path)
     recording.analyze()
+    print(recording.detections)
     
-    species = recording.detections[0]['common_name']
+    try:
+        if recording.detections and recording.detections[0]['confidence'] > 0.80:
+            species = recording.detections[0]['common_name']
+            print(f"Detected bird: {species}")
+            image_path = f"./recordings/image_{timestamp}.jpg"
+            save_to_database(timestamp, species, audio_path, image_path)
+        else:
+            print("No bird detected in the recording")
+    except IndexError:
+        print("Error: No valid detections found")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+    '''
+    if recording.detections is not None:
+        species = recording.detections[0]['common_name']
+    
+
     print(f"Detected bird {species}")
     image_path = f"./recordings/image_{timestamp}.jpg"
 
     save_to_database(timestamp, species, audio_path, image_path)
+    '''
 
--- web/app.py
+++ web/app.py
@@ -6,6 +6,7 @@ from sensors.camera_stream import generate_frames, streaming_event
 import os
 from sensors.light_sensor import read_light_sensor
 from sensors.temperature import readDHT22
+import sqlite3
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -48,6 +49,31 @@ def temp_humd_status():
         return jsonify({"temperature": temperature, "humidity": humidity})
     else:
         return jsonify({"temperature": "Error", "humidity": "Error"}), 500
+    
+@app.route('/get_latest')
+def get_latest_bird_detections():
+    conn = sqlite3.connect('./database/bird_species.db')
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        SELECT timestamp, species, image_path, audio_path
+        FROM bird_species
+        ORDER BY timestamp DESC
+        LIMIT 10
+    """)
+    latest_bird_detections = cursor.fetchall()
+
+    records = [
+        {
+            'timestamp': record[0],
+            'bird_name': record[1],
+            'audio_path': record[2],
+            'image_path': record[3]
+        } for record in latest_bird_detections
+    ]
+
+    conn.close()
+    return jsonify(records)
 
 def run_flask():
     app.run(host='0.0.0.0', port=5000, debug=False)
